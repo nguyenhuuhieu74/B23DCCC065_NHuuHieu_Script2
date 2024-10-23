3872961a3a1594750e2865f1cd50c120a415b96b
nguyenhuuhieu74 <icedcat742005@gmail.com>
2024-10-23 15:48:53 +0700

    m

---
+++ day7/express-rest-api/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
+
+app.get('/users', (req, res) => {
+    res.json([
+        { id: 1, name: 'John Doe'},
+        { id: 2, name: 'John Smith'}
+    ]);
+});
